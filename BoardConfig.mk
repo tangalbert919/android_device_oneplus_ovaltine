bdb13565c8ece94c5f3aeb614e7fa76d2537bb35
Albert Tang <tangalbert919@yahoo.com>
2023-03-22 12:04:21 -0500

    [DNM] ovaltine: Use prebuilt kernel/dtb
    Change-Id: Ica558619d1517196c56c08b729ac37a72d1337e5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,8 +10,8 @@ include device/oneplus/sm8450-common/BoardConfigCommon.mk
 DEVICE_PATH := device/oneplus/ovaltine
 
 # Kernel
-BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+#BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
+#BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_boot))
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8475
@@ -19,6 +19,22 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/sm8475
 
 # Display
 TARGET_SCREEN_DENSITY := 450
+
+# Temporary, since I can't get the source-built kernel to work
+TARGET_KERNEL_DIR := $(DEVICE_PATH)-kernel
+KERNEL_MODULE_DIR := $(TARGET_KERNEL_DIR)/modules
+
+BOARD_PREBUILT_DTBIMAGE_DIR := $(TARGET_KERNEL_DIR)
+BOARD_PREBUILT_DTBOIMAGE := $(TARGET_KERNEL_DIR)/dtbo.img
+TARGET_PREBUILT_KERNEL := $(TARGET_KERNEL_DIR)/kernel
+
+KERNEL_MODULES := $(wildcard $(KERNEL_MODULE_DIR)/*.ko)
+
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(KERNEL_MODULE_DIR)/modules.blocklist
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_MODULE_DIR)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES := $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_MODULES_LOAD)))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_MODULE_DIR)/modules.load.recovery))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)))
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
